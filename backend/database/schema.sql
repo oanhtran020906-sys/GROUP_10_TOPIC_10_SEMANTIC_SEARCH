ab36892d210a01c37fe1f82471ef216cc556d0c3
oanhtran020906-sys <oanhtran020906@gmail.com>
2026-04-07 15:31:32 +0700

    Update schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,20 +1,40 @@
+DROP DATABASE IF EXISTS semantic_search;
+CREATE DATABASE semantic_search;
+
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
 
-    category_id INTEGER REFERENCES categories(id),
+    category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
 
     name VARCHAR(255) NOT NULL,
     brand VARCHAR(100),
 
-    price DECIMAL(12, 2) NOT NULL, -- ví dụ: 11000000.00
+    price DECIMAL(12, 2) NOT NULL,
 
     image_url TEXT,
 
-    -- Mô tả chi tiết (dùng cho SQL + embedding)
     description TEXT NOT NULL,
 
-    -- Field tối ưu search (SQL + vector)
-    search_blob TEXT,
+    search_blob TEXT NOT NULL,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX idx_products_search_blob 
+ON products USING gin (search_blob gin_trgm_ops);
+
+CREATE INDEX idx_products_category_id 
+ON products(category_id);
+
+CREATE INDEX idx_products_brand 
+ON products(brand);
